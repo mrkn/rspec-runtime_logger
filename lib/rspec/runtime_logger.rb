9c8588b6169db4ee9ebd0e6ab8af8538a74126f7
Kenta Murata <mrkn@cookpad.com>
2013-09-03 20:10:27 +0900

    bundle gem rspec-runtime_logger

---
+++ lib/rspec/runtime_logger.rb
@@ -0,0 +1,7 @@
+require "rspec/runtime_logger/version"
+
+module Rspec
+  module RuntimeLogger
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/runtime_logger/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module RuntimeLogger
+    VERSION = "0.0.1"
+  end
+end
